f2f4daae9d374378795318418f6b04c022481ac5
Adhiraj7777 <119966859+Adhiraj7777@users.noreply.github.com>
2022-12-07 15:44:25 -0500

    Update AdhirajScenarios.js

--- AdhirajScenarios.js
+++ AdhirajScenarios.js
@@ -1,12 +1,30 @@
 const LoginPage = require('../pageobjects/login.page');
-const SecurePage = require('../pageobjects/secure.page');
 
-describe('Progressbar Scenario', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open();
-        await LoginPage.startButton();
-        await LoginPage.CalculateProgress();
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+describe('Progressbar, AjaxClick, HideUnHide, ShadowdomCopyPaste ButtonColorChange Scenarios', () => {
+    it('Verify Progress Bar Stop at 75 Percentage functionality', async () => {
+        await LoginPage.open(progressbar);
+        await LoginPage. verifyProgressBar();  
     });
+ 
+    it('Verify Ajax Message Clicking more than once functionality', async () => {
+        await LoginPage.open(ajax);
+        await LoginPage. verifyAjaxButtonMessages();      
+    });
+
+   it('Verify Hide and Unhide Button functionality', async () => {
+        await LoginPage.open(visibility);
+        await LoginPage.verifyHideUnhideButton();
+       await LoginPage.visibilityOfOtherElementsAfterClickonHideButton();
+    });
+
+  it('Verify copy and paste functionality', async () => {
+        await LoginPage.open(shadowdom);
+        await LoginPage.verifyCopyPasteText();   
+    });
+  
+   it('Verify Button Turns red after clicking functionality', async () => {
+        await LoginPage.open(click);
+        await LoginPage.verifyButtonChangeColor();   
+    });
+
 });
